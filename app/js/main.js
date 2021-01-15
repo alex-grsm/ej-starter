148421e199268a37ea4f7ccc4ea9213e921a56d9
Alexey <alexey.grsm@gmail.com>
2021-01-15 15:25:16 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+// import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -P jquery)
+
+// require('~/app/js/vendor/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+	console.log('done');
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,235 @@
+let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
+		fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync  = require('browser-sync').create();
+const bssi         = require('browsersync-ssi');
+const ssi          = require('ssi');
+const webpack      = require('webpack-stream');
+const sass         = require('gulp-sass');
+const sassglob     = require('gulp-sass-glob');
+const less         = require('gulp-less');
+const lessglob     = require('gulp-less-glob');
+const styl         = require('gulp-stylus');
+const stylglob     = require('gulp-empty');
+const cleancss     = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename       = require('gulp-rename');
+const imagemin     = require('gulp-imagemin');
+const newer        = require('gulp-newer');
+const rsync        = require('gulp-rsync');
+const del          = require('del');
+const sourcemaps 	 = require('gulp-sourcemaps');
+const notify 			 = require('gulp-notify');
+const ttf2woff2 	 = require('gulp-ttf2woff2');
+const fs 					 = require('fs');
+const uglify 			 = require('gulp-uglify-es').default;
+const gutil 			 = require('gulp-util');
+const ftp          = require('vinyl-ftp');
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/',
+			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+		},
+		ghostMode: { clicks: false },
+		notify: false,
+		online: true,
+		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+	})
+}
+
+const fonts = () => {
+  return src('./app/fonts/src/**.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest('./app/fonts/dist/'));
+}
+
+const checkWeight = (fontname) => {
+  let weight = 400;
+  switch (true) {
+    case /Thin/.test(fontname):
+      weight = 100;
+      break;
+    case /ExtraLight/.test(fontname):
+      weight = 200;
+      break;
+    case /Light/.test(fontname):
+      weight = 300;
+      break;
+    case /Regular/.test(fontname):
+      weight = 400;
+      break;
+    case /Normal/.test(fontname):
+      weight = 400;
+      break;
+    case /Medium/.test(fontname):
+      weight = 500;
+      break;
+    case /SemiBold/.test(fontname):
+      weight = 600;
+      break;
+    case /Demi/.test(fontname):
+      weight = 600;
+      break;
+    case /Semi/.test(fontname):
+      weight = 600;
+      break;
+    case /Bold/.test(fontname):
+      weight = 700;
+      break;
+    case /ExtraBold/.test(fontname):
+      weight = 800;
+      break;
+    case /BoldItalic/.test(fontname):
+      weight = 800;
+      break;
+    case /UltraBold/.test(fontname):
+      weight = 800;
+      break;
+    case /Heavy/.test(fontname):
+      weight = 800;
+      break;
+    case /Black/.test(fontname):
+      weight = 900;
+      break;
+    default:
+      weight = 400;
+  }
+  return weight;
+}
+
+function scripts() {
+	return src(['app/js/*.js', '!app/js/*.min.js'])
+		.pipe(webpack({
+			mode: 'production',
+			output: {
+				filename: 'main.js',
+			},
+			module: {
+				rules: [
+					{
+						test: /\.(js)$/,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader',
+						query: {
+							presets: ['@babel/env'],
+							plugins: ['babel-plugin-root-import']
+						}
+					}
+				]
+			}
+		})).on('error', function handleError() {
+			this.emit('end');
+		})
+
+    .pipe(sourcemaps.init())
+		.pipe(uglify().on("error", notify.onError()))
+		.pipe(rename('main.min.js'))
+    .pipe(sourcemaps.write('.'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream());
+}
+
+function styles() {
+	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
+		.pipe(sourcemaps.init())
+		.pipe(eval(`${preprocessor}glob`)())
+		.pipe(eval(preprocessor)()).on("error", notify.onError())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 5 versions'], grid: true, cascade: false }))
+		.pipe(cleancss({ level: { 2: { specialComments: 0 } }, /* format: 'beautify' */ }))
+		.pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write('.'))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
+
+function images() {
+	return src(['app/images/src/**/*'])
+		.pipe(newer('app/images/dist'))
+		.pipe(imagemin())
+		.pipe(dest('app/images/dist'))
+		.pipe(browserSync.stream())
+}
+
+function buildcopy() {
+	return src([
+		'{app/js,app/css}/*.min.*',
+		'app/images/**/*.*',
+		'!app/images/src/**/*',
+		'!app/css/*.map',
+		'!app/js/*.map',
+		'app/fonts/**/*',
+		'!app/fonts/src/*',
+		'app/resources/**/*'
+	], { base: 'app/' })
+	.pipe(dest('dist'))
+}
+
+async function buildhtml() {
+	let includes = new ssi('app/', 'dist/', '/**/*.html')
+	includes.compile()
+	del('dist/parts', { force: true })
+	del('dist/fonts/src', { force: true })
+}
+
+function cleandist() {
+	return del('dist/**/*', { force: true })
+}
+
+// deploy SFTP
+function deploySftp() {
+	return src('dist/')
+		.pipe(rsync({
+			root: 'dist/',
+			hostname: 'username@yousite.com',
+			destination: 'yousite/public_html/',
+			include: [/* '*.htaccess' */], // Included files to deploy,
+			exclude: [ '**/Thumbs.db', '**/*.DS_Store' ],
+			recursive: true,
+			archive: true,
+			silent: false,
+			compress: true
+		}))
+}
+
+// deploy FTP
+const deploy = () => {
+  let conn = ftp.create({
+    host: '',
+    user: '',
+    password: '',
+    parallel: 10,
+    log: gutil.log
+  });
+
+  let globs = [
+		'dist/**'
+  ];
+
+  return src(globs, {
+      // base: '.',
+      buffer: false
+    })
+   .pipe(conn.newer('/public_html')) // only upload newer files
+   .pipe(conn.dest('/public_html'));
+}
+
+
+function startwatch() {
+	watch(`app/styles/${preprocessor}/**/*`, { usePolling: true }, styles)
+	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
+	watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images)
+	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+}
+
+exports.scripts = scripts
+exports.styles  = styles
+exports.images  = images
+exports.fonts   = fonts
+exports.deploy  = deploy
+exports.deploySftp = deploySftp;
+exports.assets  = series(scripts, styles, images)
+exports.build   = series(cleandist, scripts, styles, images, buildcopy, buildhtml)
+exports.default = series(scripts, styles, images, parallel(browsersync, startwatch))
